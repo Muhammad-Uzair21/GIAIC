63fa5f18907fc1ca07140194b4f543c060f5bcb1
Muhammad Uzair <uzair.jay21@gmail.com>
2024-10-28 00:29:38 +0500

    updated type of socialimg prop

--- nextjs/Assignments/Assignment_three/app/components/InstaImg.tsx
+++ nextjs/Assignments/Assignment_three/app/components/InstaImg.tsx
@@ -2,15 +2,28 @@ import React from "react";
 import Image from "next/image";
 import { FaInstagram } from "react-icons/fa6";
 
-const InstaImg =({socialimg})=>{
-    return(
-    <div className="relative">
-        <Image src={socialimg} alt="/" className="w-full h-full" layout="resonsive"/>
-        <div className=" flex justify-center items-center absolute top-0 left-0 bottom-0 right-0 hover:bg-black/50 group ">
-        <p className="text-gray-300 hidden group-hover:block cursor-pointer "><FaInstagram size={30}/></p>
-        </div>
-    </div>
-    )
+interface InstaImgProps {
+  socialimg: string;
 }
 
-export default InstaImg
+const InstaImg: React.FC<InstaImgProps> = ({ socialimg }) => {
+  return (
+    <div className="relative">
+      <Image
+        src={socialimg}
+        alt="Instagram Image"
+        className="w-full h-full"
+        layout="responsive"
+        width={500}
+        height={500}
+      />
+      <div className="flex justify-center items-center absolute top-0 left-0 bottom-0 right-0 hover:bg-black/50 group">
+        <p className="text-gray-300 hidden group-hover:block cursor-pointer">
+          <FaInstagram size={30} />
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default InstaImg;
