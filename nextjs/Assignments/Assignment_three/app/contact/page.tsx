f3583665c43704c58553c43dc6eeb050e9a2765f
Muhammad Uzair <uzair.jay21@gmail.com>
2024-10-27 23:51:40 +0500

    updated the font import

--- nextjs/Assignments/Assignment_three/app/contact/page.tsx
+++ nextjs/Assignments/Assignment_three/app/contact/page.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 import Hero from "../components/Hero";
 import Form from "../components/Form";
 
--- nextjs/Assignments/Assignment_three/app/page.tsx
+++ nextjs/Assignments/Assignment_three/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
 import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
 import Slider from "./components/Slider";
 import Insta from "./components/Insta";
 import { SliderData } from "./components/SliderData";
-import Footer from "./components/Footer";
 export default function Home() {
   return (
     <>
